df853fef9d972b140f7f8cd6299ba07068c43f49
miruthulashree-2024-aia <miruthulashrees44@gmail.com>
2024-11-03 21:58:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,3 @@
-####create your own (challenge)
-
-import streamlit as st
-
-#function to feedback page
-
-def feedback(session):
-    st.write("Why is this {session} your favourite?")
-    reason=st.text_input("because...")
-    pass 
-    st.write("Why is this {session} not your favourite?")
-    reason=st.text_input("because...",key="reason_2")
-    st.write("Finally click the submit button to record your response")
-    if st.button("submit"):
-        st.session_state.feedback={"session":session,"reason":reason}  
-        st.rerun()  
-        
-       
-             
-        
-#Main page
-st.header("Interactive Feedback Page")   
-st.write("welcome to our Interactive Feedback Page!!!.This is a fun and educational way where we were able to find our strength and weekness which helps us to improve our communication skills .let's collaborate and see what kinds of feedbacks we are getting!")  
-if "feedback"  not in st. session_state:
-    st.write("feedback for the session:")
-    st.write("Click the option A and choose the question and enter the reason for choosing it")
-    if st.button("option A"):
-        feedback("option A")
 
                 
 ## PORTFOLIO ("Using streamlit") 
